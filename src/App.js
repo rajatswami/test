b32951c29d952a7414afe44191438d9571663a65
rajatswami <rajatswami219@gmail.com>
2026-01-17 11:12:48 +0530

    initial react project

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import Alert from './components/Alert';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({ msg: message, type: type });
+    setTimeout(() => setAlert(null), 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode is enabled", "success");
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode is enabled", "success");
+    }
+  };
+
+  return (
+    <React.Fragment>
+    {/* <Router> */}
+      <Navbar title="TextUtils"  mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+
+      <div className="container my-3">
+        {/* <Routes> */}
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* <Route path="/" element={ */}
+            <TextForm showAlert={showAlert} heading="Enter the text to analyze Below" mode={mode} />
+            // 
+        {/* </Routes> */}
+      </div>
+    {/* </Router> */}
+    </React.Fragment>
+  );
+}
+
+export default App;
